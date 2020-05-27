4869ea10bb2c583779148cdaaba5cc42c66d7bf4
suthepkoh <patchara.oan@gmail.com>
2020-05-28 00:02:50 +0700

    Test

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ export default class App extends Component {
           this.count++;
           this.setState({clock: this.state.clock + 1 })
           console.log("Count: " + this.count);
-          <h1>qweqweqweq</h1>
+          <h1>Test</h1>
         }}>count {this.count}</h1>
       </div>
     );
